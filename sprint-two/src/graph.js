d972388c5e6ef599ed879288d51159db505cecd0
davydhong <davydhong@gmail.com>
2018-08-02 21:00:18 -0700

    completed graph, set, tree

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,35 +1,68 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function () {
+    this.edges = [];
+    this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function (node) {
+    this.nodes[this.nodes.length] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function (node) {
+    if (_.contains(this.nodes, node)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function (node) {
+    if (_.contains(this.nodes, node)) {
+        this.nodes = _.reject(this.nodes, function (e) {
+            return e === node;
+        });
+        this.edges = _.reject(this.edges,function(e){
+            return e[0] === node || e[1] === node;
+        });
+    }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
+Graph.prototype.hasEdge = function (fromNode, toNode) {
+    if (this.edges.toString().indexOf([fromNode, toNode].toString()) > -1 || this.edges.toString().indexOf([toNode, fromNode].toString()) > -1) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
+Graph.prototype.addEdge = function (fromNode, toNode) {
+    if (_.contains(this.nodes, fromNode) && _.contains(this.nodes, toNode)) {
+        this.edges.push([fromNode, toNode]);
+    }
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+Graph.prototype.removeEdge = function (fromNode, toNode) {
+    if(this.hasEdge(fromNode,toNode)){
+        this.edges = _.reject(this.edges, function (e) {
+            return e.toString() === [fromNode,toNode].toString();
+        });
+    }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
+Graph.prototype.forEachNode = function (cb) {
+    this.nodes = _.map(this.nodes,function(e){
+        return cb(e);
+    });
 };
 
 /*
@@ -37,3 +70,4 @@ Graph.prototype.forEachNode = function(cb) {
  */
 
 
+// slice(0,length -1)   slice(numarr[])
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,69 @@
-var Tree = function(value) {
+
+
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = null; 
+  expand(newTree, treeMethods)
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+  var newChild = Tree(value);
+  if (this.children === null) {
+    this.children = [newChild];
+  } else {
+    this.children[this.children.length] = newChild;
+  }
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.children !== null) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+    return false;
+  };
 };
+
+var expand = function (obj, methods) {
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
+}
+
+
+//   var testTree = Tree(5);
+//   testTree.addChild(1);
+//   testTree.addChild(2);
+//   testTree.addChild(3);
+//   testTree.addChild(4);
+//   testTree.children[0].addChild(5);
+//   testTree.children[1].addChild(6);
+//   testTree.children[2].addChild(7);
+//   testTree.children[2].addChild(17);
+//   testTree.children[0].children[0].addChild(15);
+//   testTree.children[0].children[0].children[0].addChild(115);
+
+  
+// console.log( testTree.contains(115) );
+
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+ 
+  Response: Linear,  O(n)
+*/
