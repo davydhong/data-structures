6e3d728e7793f9ddbb500b7aec588b449c8a748e
davydhong <davydhong@gmail.com>
2018-08-04 12:21:18 -0700

    finished hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,51 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    this._storage.get(index).push([k, v]);
+  }
+
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var result;
+
+  this._storage.get(index).forEach(function (arr) {
+    if (arr[0] === k) {
+      result = arr[1];
+    }
+  });
+  return result;
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var deleted;
+  this._storage.set(index, this._storage.get(index).filter(function (element) {
+    if (element[0] === k) {
+      deleted = element[1];
+    }
+    return element[0] !== k;
+  }));
+  return deleted;
 };
 
+// a = [1,2,3,4,5];
+// for (var i = 0 ;)
 
+
+// var HT = new HashTable('height', 5);
+// HT.retrieve('height');
 
 /*
  * Complexity: What is the time complexity of the above functions?
